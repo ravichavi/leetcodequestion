227be377525e9e24cbea862d14df8058d95119be
chavibansal <chhavibansal19082006@gmail.com>
2026-06-13 16:48:39 +0530

    Solved the twop sum problem

---
+++ TWOSUM.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(auto it=nums.begin(); it != nums.end(); ++it) {
+            for(auto jt=it + 1; jt != nums.end(); jt++) {
+                if (*it + *jt == target) {
+                    return vector<int>{(int)(it - nums.begin()), (int)(jt - nums.begin())};
+                }
+            }
+        }
+        return {};
+    }
+};
+int main(){
+    Solution sol;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = sol.twoSum(nums, target);
+    for(auto i : result) {
+        cout << i << " ";
+    }
+    return 0;
+}
