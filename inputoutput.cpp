c1b2ba0d4ef9c567624e82251732ad9ae3170c23
chavibansal <chhavibansal19082006@gmail.com>
2026-06-13 17:03:15 +0530

    Added inputoutput problem

---
+++ inputoutput.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void printNumber() {
+          int a;
+          cin >> a;
+          cout << a<< endl;
+    }
+};
+int main() {
+    Solution sol;
+    sol.printNumber();
+    return 0;
+}
